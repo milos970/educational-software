4ab3421268dc0a397bb7d256636224f956f825be
milos970 <dev980lop@gmail.com>
2023-11-19 22:46:39 +0100

    Add jacobi and seidel methods

--- src/main/java/com/milos/numeric/controllers/CatergoryController.java
+++ src/main/java/com/milos/numeric/controllers/CatergoryController.java
@@ -3,10 +3,7 @@ package com.milos.numeric.controllers;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class CatergoryController {
-
-
-
-
+public class CatergoryController
+{
 
 }
