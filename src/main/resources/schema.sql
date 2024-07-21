21b968cf714cbe7ea5d34c0a2af0902b6107da87
Milos <dev980lop@gmail.com>
2024-07-21 16:43:55 +0200

    Modified for github

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,4 +1,4 @@
-/*delete from system_settings
+delete from system_settings
 delete from message
 delete from chat
 delete from verification_token
@@ -6,4 +6,4 @@ delete from employee
 delete from student
 delete from personal_info
 delete from material
-*/
+
